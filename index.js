fe66b5526575f336cd5094d5a14fb7950725e560
Dara <58168757+DaraNel@users.noreply.github.com>
2021-01-11 22:56:38 +0100

    update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const studentsData = [
     name: "Dara Nelson",
     profession: "Computer Science",
     favLangs: ["HTML", "CSS","JavaScript", "PHP"],
-    quotes: "<strong>QUOTES:</strong> The technology you use impresses no one. The experience you create with it is everything. – <strong>Sean Gerety</strong>",
+    quotes: "<strong>QUOTES:</strong> If you think responsive’s simple, I feel bad for you son. We got 99 viewports, but the iPhone’s just one. - <strong>Josh Brewer</strong>",
     photo: "https://res.cloudinary.com/daranel/image/upload/v1606746079/DN_k6qhhg.jpg"
   },
   {
@@ -17,7 +17,7 @@ const studentsData = [
     name: "Jennifer Iroh",
     profession: "Front End Developer",
     favLangs: ["HTML", "CSS","JavaScript", "PHP"],
-    quotes: "<strong>QUOTES:</strong> Technology is nothing. What’s important is that you have a faith in people, that they’re basically good and smart, and if you give them tools, they’ll do wonderful things with them.” – <strong>Steve Jobs</strong>",
+    quotes: "<strong>QUOTES:</strong> Technology is nothing. What’s important is that you have faith in people, that they’re good and smart, give them tools, they’ll do wonderful things.” – <strong>Steve Jobs</strong>",
     photo: "https://res.cloudinary.com/iroh-jenny/image/upload/w_1000,ar_1:1,c_fill,g_auto,e_art:hokusai/v1606586839/Jennifer_rlckom.jpg "
   },
   {
@@ -36,29 +36,29 @@ const studentsData = [
   },
   {
     name: "Christianah Adeyemi",
-    profession: "Lawyer",
-    favLangs: ["HTML", "CSS","JavaScript", "PHP"],
+    profession: "Law",
+    favLangs: ["HTML", "CSS", "JavaScript"],
     quotes: "<strong>QUOTES:</strong> Technology is best when it brings people together  – <strong>Matt Mullenweg</strong>",
     photo: "https://res.cloudinary.com/daranel/image/upload/v1610186688/Christianah_pxsmmb.jpg "
   }
 ];
 
-curIndex = 0;
-    imgDuration = 2000;        
+// curIndex = 0;
+//     imgDuration = 2000;        
 
-var home = document.getElementById('app')
-var image = document.createElement('img')
+// var home = document.getElementById('app')
+// var image = document.createElement('img')
 
-function slideShow() {
-    if(curIndex != petsData.length-1) { 
-      image.src = petsData[curIndex];
-      app.appendChild(image)
-      curIndex++;
-    }else{
-      curIndex = 0;
-    } 
-}
-setInterval(slideShow,2000)
+// function slideShow() {
+//     if(curIndex != petsData.length-1) { 
+//       image.src = petsData[curIndex];
+//       app.appendChild(image)
+//       curIndex++;
+//     }else{
+//       curIndex = 0;
+//     } 
+// }
+// setInterval(slideShow,2000)
 
 function foods(foods) {
   return `
@@ -72,7 +72,7 @@ ${foods.map(food => `<li>${food}</li>`).join("")}
 function studentTemplate(student)  {
   return `
   <div class ="student">
-  <img class="student-photo" src="${student.photo}">
+  <img class="student-photo" width="600" height="600" src="${student.photo}">
   <h2 class="student-name">${student.name} <span class="profession">(${student.profession})</span></h2>
   ${student.favLangs ? foods(student.favLangs) : ""}
   <p class="student-quotes">${student.quotes}</p>
@@ -83,5 +83,5 @@ function studentTemplate(student)  {
 document.getElementById("app").innerHTML = `
     <h1 class="app-title">Pearls Africa Photobook</h1>
     ${studentsData.map(studentTemplate).join('')}
-    <p class="footer">These ${studentsData.length} students of LAW 1 were added recently. Check back soon for updates: <strong>Dara Nelson, 2020</strong></p>
+    <p class="footer">These ${studentsData.length} students of LAW 1 were added recently. Check back soon for updates: <strong>Copyright &copy;Dara Nelson, 2020</strong></p>
 `;
